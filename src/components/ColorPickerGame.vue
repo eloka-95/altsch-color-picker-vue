fb7d01c9cc93e2eefe0afda7fd8dbdb53c6f20cf
eloka-95 <emeholisaeloka@gmail.com>
2023-02-06 15:40:26 +0100

    added work file

---
+++ src/components/ColorPickerGame.vue
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <button v-for="color in colors" :key="color" @click="matchColor(color)">
+      {{ color }}
+    </button>
+  </div>
+</template>
+<script>
+  import colorPickerLogic from "../composables/Color-picker";
+export default {
+  setup() {
+    const { colors, message, matchColor } = colorPickerLogic();
+    return {
+      colors,
+      message,
+      matchColor,
+    };
+  },
+};
+
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/composables/Color-picker.js
@@ -0,0 +1,17 @@
+
+import { ref, reactive } from "@vue/reactivity";
+
+export default function colorPickerLogic () {
+
+    const colors = ["green", "red", "blue", "purple"];
+    const message = ref("Pick a color...");
+    const matchColor = (value) => {
+      const randomNumber = Math.floor(Math.random() * 3) + 1;
+      if (colors[randomNumber] === value) {
+        message.value = `You win... [answer: ${colors[randomNumber]}]`;
+        return;
+      }
+      message.value = `You loose [answer: ${colors[randomNumber]}]`;
+    };
+    return { colors, message, matchColor };
+};
